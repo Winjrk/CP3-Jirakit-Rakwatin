1f7f0f74a8a6b70dcb873a8daa0f1a9958b1aee2
Winjrk <60658153+Winjrk@users.noreply.github.com>
2020-05-09 23:45:22 +0700

    Add files via upload

---
+++ Exercise4_Jirakit_R.py
@@ -0,0 +1,9 @@
+Eng = 100
+Bus = 101
+ComSys = 102
+ComPro = 103
+print("--- Your Score ---")
+print("Foundation English :"),Eng
+print("General Business :"),Bus
+print("Introduction to Computer Systems :"),ComSys
+print ("Computer Programming :"),ComPro
